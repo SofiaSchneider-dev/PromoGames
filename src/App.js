0649b0bd853d8632a09017bfae350b3e09dac275
Sofia Schneider Jimenez <90013687+sofiasj02@users.noreply.github.com>
2024-04-28 23:37:08 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import LandingPage from "./LandingPage";
 import MainApp from "./MainApp";
 
 const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(
+    JSON.parse(localStorage.getItem("isLoggedIn")) || false
+  );
+
+  useEffect(() => {
+    localStorage.setItem("isLoggedIn", JSON.stringify(isLoggedIn));
+  }, [isLoggedIn]);
 
   const handleLogin = () => {
     setIsLoggedIn(true);
